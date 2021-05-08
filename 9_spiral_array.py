1890d1a0afd9f7b3abd880866cf90d3042f640ed
Steve <nontile@daum.net>
2021-05-08 22:19:13 +0900

    init

--- 9_spiral_array.py
+++
@@ -1,7 +0,0 @@
-n, m = map(int, input().split())
-
-arr = [[0 for in range(m)] for _ in range(n)]
-
-print(arr)
-
-
--- Test.java
+++ Test.java
@@ -1,28 +1,5 @@
-import java.util.Arrays;
 class Test {
-   public static void main(String[] arg){
-
-       int[] arr = {5, 3, 1,1,3}; 
-       int[] dp = new int[arr.length +1];
-       
-       Arrays.sort(arr);
-
-    //    dp[0] = arr[0]
-    //    int k =2;
-       for(int i =0; i<arr.length + 1 ; i++){
-           if(i+1 < arr.length){
-
-             dp[i] = arr[i +1] - arr[i];
-           }
-         System.out.println(dp[i]);
-       }
-
-       Arrays.sort(dp);
-        for(int i =dp.length; i>0 ; i--){
-           
-         System.out.println(dp[i]);
-       }
-
-
-   }
+    public static void main(String args[]){
+        System.out.println("hello");
+    }
 }
